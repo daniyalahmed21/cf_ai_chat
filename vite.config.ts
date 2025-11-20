7d16cc5b64b9f5a01eb225a6e21c11f630e7df96
Daniyal Ahmed <126581974+daniyalahmed21@users.noreply.github.com>
2025-11-20 23:43:13 +0000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,4 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'image.png') {
-            return '[name][extname]'
-          }
-          return 'assets/[name]-[hash][extname]'
-        }
-      }
-    }
-  },
-  publicDir: 'public'
 })
